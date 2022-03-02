fedb024838a1a9559dbeef40f98fb01cba70e988
DieFleitas <diegofleitas98@outlook.es>
2022-03-02 16:18:02 -0300

    navbar section

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import GlobalStyle from './globalStyles';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <Router>
-      <GlobalStyle />
-      <Switch>
-        <Route path='/' exact component={HomePage}/>
-      </Switch>
-      
-    </Router>
-  );
+	return (
+		<Router>
+			<GlobalStyle />
+			<Navbar />
+			<Switch>
+				<Route path="/" exact component={HomePage} />
+			</Switch>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
