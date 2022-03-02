39f07c7a0cf01d03aeec42db2064fca801999fc1
DieFleitas <diegofleitas98@outlook.es>
2022-03-02 14:30:20 -0300

    create-react-app & readme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import GlobalStyle from './globalStyles';
-import HomePage from './pages/HomePage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<Router>
-			<GlobalStyle />
-			<Navbar />
-			<Switch>
-				<Route path="/" exact component={HomePage} />
-			</Switch>
-			<Footer />
-		</Router>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/data/ClientsData.js
+++
@@ -1,7 +0,0 @@
-export const clientsData = [
-	[{ name: 'airbnb' }],
-	[{ name: 'amazon' }, { name: 'apple' }],
-	[{ name: 'coca-cola' }, { name: 'youtube' }, { name: 'google' }],
-	[{ name: 'microsoft' }, { name: 'netflix' }],
-	[{ name: 'facebook' }],
-];
